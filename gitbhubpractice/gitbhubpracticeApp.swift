3a809cd61947d33a429249749b170b4a0eb323d2
Sophie <nguyensophie721@gmail.com>
2023-08-01 11:11:30 -0400

    Initial Commit

---
+++ gitbhubpractice/gitbhubpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitbhubpracticeApp.swift
+//  gitbhubpractice
+//
+//  Created by scholar on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct gitbhubpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
